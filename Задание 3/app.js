1315df461efbd2de58d0c3ec1d74d5cbaf294141
arman1006 <bezoyan9033718038@gmail.com>
2024-01-25 10:35:39 +0300

    Выполнили работу с многомерными массивами

---
+++ Задание 3/app.js
@@ -0,0 +1,3 @@
+let students = [ ['Mac', 'BurgerKind', 'KFC', 'Dodo Pizza', 'блин бери'],['Apple', 'MicroSoft', 'Linux','Windows','Java'],['OSTIN','H&M','Adidas','Nike','Puma'],['BMW','Mercedes','LADA','Audi','Toyota'] ];
+console.log(students);
+console.log(students[3][0]);
