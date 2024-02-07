72b236d0ae9367a9fbe54cd17fee89f47b2a81fb
arman1006 <bezoyan9033718038@gmail.com>
2024-02-07 10:48:21 +0300

    наследование классов

--- задание 9/main.js
+++ задание 9/main.js
@@ -1,22 +1,60 @@
-//Работа с клcac
-class NumCheck{
-    constructor(array){
-    this.array = array;
-    }
-    
-    filter(){
-        return this.array.some(num => num % 3 === 0);
-    }
+//Работа с клcacс
+//class Person{
+//constructor(name, age){
+//    this.name = name;
+ //   this.age = age;
+//}
+//print(){
+//    console.log(`Имя: ${this.name}, возраст: ${this.age}`);
+//}
+//}
 
-    print() {
-        if (this.filter()) {
-            console.log("Да");
-        } else {
-            console.log("Нет");
-        }
-    }
-}
+//class Employee extends Person{
+//    constructor(company){
+ //       this.company = company;
+ //   }
 
-const numbers = [1, 2, 4, 1, 9, 10];
-const numCheck = new NumCheck(numbers);
-numCheck.print();
+  //  print(){
+       // console.log(`${this.name} работает в ${this.company}`);
+  //  }
+//}
+
+//const tom = new Person("Harry", 17);
+//tom.print();
+
+//const company = new Employee("Россети");
+//company.print();
+class Person { 
+    name; 
+    age; 
+ 
+    constructor(pName, pAge) { 
+        this.name = pName; 
+        this.age = pAge; 
+    } 
+ 
+    print() { 
+        console.log(this.name); 
+    } 
+} 
+ 
+ 
+class Company extends Person { 
+    company; 
+ 
+    constructor(e, pName, pAge) { 
+        super(pName, pAge); 
+        this.company = e; 
+    } 
+ 
+    work() { 
+        console.log(`${this.name} работает в компании ${this.company}`); 
+    } 
+} 
+ 
+ 
+const tom = new Person("Tom", 28); 
+const work = new Company("Россети", "Tom", 28); 
+ 
+tom.print(); 
+work.work();
