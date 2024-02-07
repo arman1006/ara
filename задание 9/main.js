420431d9ab76d6ea1eed112151e785987abd230c
arman1006 <bezoyan9033718038@gmail.com>
2024-02-07 09:43:26 +0300

    Задание 5 правильное

--- задание 9/main.js
+++ задание 9/main.js
@@ -1,23 +1,34 @@
 //Работа с клcac
 class Filter{
-    inputNumbers;
+    i;
     resultFilter;
 
-    constructor(numbers){
-    this.inputNumbers = numbers;
-    }
+    
 
     filter(){
-        return this.inputNumbers.filter(number =>number % 3 === 0 || number % 5 === 0 || number % 7 === 0);
-        
+        this.i = [];
+        for (let i = 0; i <= 100; i++){
+            if(i % 3 === 0 || i % 5 === 0 || i % 7 === 0){
+                this.i.push(i);
+            } 
+               
+
+        }
+        return this.i;
+       
     }
-    print(){
+    print() {
         this.resultFilter = this.filter();
         console.log(this.resultFilter);
     }
+      
+    
+
+
+   
 
 }
-const inputNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const resultArrays = new Filter(inputNumbers);
-resultArrays.print();
+const filter = new Filter();
+filter.print();
+
 
